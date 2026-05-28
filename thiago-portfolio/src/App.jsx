98982e5aa5ab8ccf30bfa235fb081879455200eb
tiagoh27 <tiagoh285@gmail.com>
2026-05-28 12:51:49 -0300

    Add files via upload

---
+++ thiago-portfolio/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-red-500">
+        Thiago Portfolio
+      </h1>
+    </div>
+  )
+}
