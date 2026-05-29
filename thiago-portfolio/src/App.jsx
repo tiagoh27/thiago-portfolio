fa2e15eb901d66106f2d86796ebcad25a721aaec
tiagoh27 <tiagoh285@gmail.com>
2026-05-28 21:13:10 -0300

    Update App.jsx

--- thiago-portfolio/src/App.jsx
+++ thiago-portfolio/src/App.jsx
@@ -70,7 +70,7 @@ export default function App() {
           <div className="bg-zinc-900 border border-green-500 rounded-3xl overflow-hidden shadow-[0_0_25px_#00ff88]">
 
             <img
-              src="https://i.imgur.com/T2ooTa3.png"
+              src="https://i.imgur.com/IL1PBD0.png"
               alt="Dark Network"
               className="w-full"
             />
